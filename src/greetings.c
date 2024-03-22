5647c30202257e95e47de21aee56d2a77497055e
Florencia Schroder <florencia.schroder@gmail.com>
2024-03-22 15:27:10 -0300

    cmake -GNinja .. funciona

---
+++ src/greetings.c
@@ -0,0 +1,5 @@
+#include "greetings.h"
+#include <stdio.h>
+
+void greetings()
+{    printf("Hello from another file!\n");}
---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+Diferencia entre 
+    #include <header1.h>
+    #include "header2.h"
+*/
+
+#include <greetings.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello, OS II. Welcome to 2024 course!|\n");
+    greetings();
+    return 0;
+}
